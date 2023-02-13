076062f70dbcca8bfd4e74b1f04f0ddc5a251516
VadimKTS <123896357+VadimKTS@users.noreply.github.com>
2023-02-13 13:26:24 +0300

    + task 3

--- Lesson5HomeWork/Program.cs
+++ Lesson5HomeWork/Program.cs
@@ -32,7 +32,7 @@ namespace Lesson5HomeWork
             //    }
             //    else if (char.IsPunctuation(charArrayFromString[i]))
             //    {
-                    
+
             //    }
             //    else
             //    {
@@ -60,14 +60,14 @@ namespace Lesson5HomeWork
 
             // task 3                     --------------------------------------
 
-            //string myStringForTask3 = "«Мы находимся здесь, чтобы внести свой вклад в этот мир. Иначе зачем мы здесь?» Стив Джобс 978345 fghj ffffff  ";
+            //string myStringForTask3 = "«Мы находимся здесь, чтобы внести bbbbbb свой вклад в этот мир. Иначе зачем мы здесь?» Стив Джобс ffffff ab f 978345 fghj  ";
             //myStringForTask3 = myStringForTask3.Trim();
             //char[] charArrayFromString = myStringForTask3.ToArray();
             //int spacesInString = 0;
 
-            //for ( int i = 0; i < myStringForTask3.Length; i++ )
+            //for (int i = 0; i < myStringForTask3.Length; i++)
             //{
-            //    if ( char.IsWhiteSpace(charArrayFromString[i]) )
+            //    if (char.IsWhiteSpace(charArrayFromString[i]))
             //    {
             //        spacesInString++;
             //    }
@@ -76,23 +76,21 @@ namespace Lesson5HomeWork
             //int[] lettersInWordsArrary = new int[spacesInString + 1];
             //int b = 0;
             //int lettersInWords = 0;
-            //for ( int i = 0; i < myStringForTask3.Length; i++ )
+            //for (int i = 0; i < myStringForTask3.Length; i++)
             //{
-            //    if (char.IsWhiteSpace(charArrayFromString[i]) )
+            //    if (char.IsWhiteSpace(charArrayFromString[i]))
             //    {
-            //        //Console.Write(lettersInWords);
             //        lettersInWordsArrary[b] = lettersInWords;
             //        lettersInWords = 0;
             //        b++;
             //    }
             //    else if (i == myStringForTask3.Length - 1)
             //    {
-            //        //Console.Write(lettersInWords);
             //        lettersInWordsArrary[b] = lettersInWords + 1;
             //    }
             //    else if (char.IsPunctuation(charArrayFromString[i]))
             //    {
-                    
+
             //    }
             //    else
             //    {
@@ -110,23 +108,66 @@ namespace Lesson5HomeWork
             //int k = 0;
             //for (int n = 0; n < charArrayFromString.Length; n++)
             //{
-            //    if ( char.IsLetter(charArrayFromString[n]) || char.IsDigit(charArrayFromString[n]) )
+            //    if (char.IsLetter(charArrayFromString[n]) || char.IsDigit(charArrayFromString[n]))
             //    {
             //        arrayOfWordsFromChars[j][k] = charArrayFromString[n];
-            //        Console.Write($"{arrayOfWordsFromChars[j][k]} ");
+            //        //Console.Write($"{arrayOfWordsFromChars[j][k]} ");
             //        k++;
             //    }
-            //    else if ( char.IsWhiteSpace(charArrayFromString[n]) )
+            //    else if (char.IsWhiteSpace(charArrayFromString[n]))
             //    {
-            //        Console.WriteLine();
+            //        //Console.Write("\t");
             //        j++;
             //        k = 0;
             //    }
+            //}
+            //// Решение
+            
+            //int[] equalChars = new int[arrayOfWordsFromChars.Length];
+            //for (int i = 0; i < arrayOfWordsFromChars.Length; i++)
+            //{
+            //    k = 0;
+            //    for (j = 0; j < arrayOfWordsFromChars[i].Length; j++)
+            //    {
+            //        for (int n = 0; n < arrayOfWordsFromChars[i].Length; n++)
+            //        {
+            //            if (arrayOfWordsFromChars[i][j].Equals(arrayOfWordsFromChars[i][n]) && j == (arrayOfWordsFromChars[i].Length - 1))
+            //            {
+            //                k++;
+            //                equalChars[i] = k;
+            //            }
+            //            else if (j == (arrayOfWordsFromChars[i].Length - 1) && i == (arrayOfWordsFromChars[i].Length - 1))
+            //            {
+            //                equalChars[i] = k;
+            //            }
+            //            else if (arrayOfWordsFromChars[i][j].Equals(arrayOfWordsFromChars[i][n]))
+            //            {
+            //                k++;
+            //            }
+            //        }
+            //    }
+            //}
+            //int maxEquals = 0;
+            //int itemWithmaxEquals = 0;
+            //for (int i = 0; i < equalChars.Length; i++)
+            //{
+            //    if (equalChars[i] > maxEquals)
+            //    {
+            //        maxEquals = equalChars[i];
+            //        itemWithmaxEquals = i;
+            //    }
+            //}
+
+            //Console.Write("\nСлово, в котором число различных символов минимально: ");
+            //for (int i = 0; i < arrayOfWordsFromChars[itemWithmaxEquals].Length; i++)
+            //{
+            //    Console.Write(arrayOfWordsFromChars[itemWithmaxEquals][i]);
             //}
 
 
             // task 4*                    --------------------------------------
 
+            //string myStringForTask4 = "«Если тебе тяжело, значит ты поднимаешься в гору. Если тебе легко, значит ты летишь в пропасть». Генри Форд ololo";
 
 
 
